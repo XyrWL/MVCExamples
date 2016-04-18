eb4499d5308f9680d1f6f0ea3db0d40e40f5b4d1
XyrWL <peter.trullenque@gmail.com>
2016-04-18 15:27:59 +0200

    Routing commit.

---
+++ RoutingProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoutingProject.Startup))]
+namespace RoutingProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
