a36d5c6b3f416c1fb9c90fac1f6a568f00c17844
XyrWL <peter.trullenque@gmail.com>
2016-04-26 15:46:51 +0200

    Added sandbox.

---
+++ Sandbox/Controllers/FirstController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sandbox.Models;
+
+namespace Sandbox.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            var model = new FirstModel();
+            model.Initiate();
+            return View(model);
+        }
+
+        // POST: First
+        [HttpPost]
+        public ActionResult Index(FirstModel model)
+        {
+            if (model.IsWinner())
+                ViewBag.Result = "You win.";
+            ModelState.Remove("CurrentValue");
+            return View(model);
+        }
+    }
+}
---
+++ Sandbox/Models/FirstModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sandbox.Models
+{
+    public class FirstModel
+    {
+        public int CurrentValue { get; set; }
+        public int ChosenNumber { get; set; }
+
+        public bool IsWinner()
+        {
+            CurrentValue += ChosenNumber;
+            if (CurrentValue >= 21)
+                return true;
+            return false;
+        }
+
+        public void Initiate()
+        {
+            //Random for who starts, assign starting value
+            CurrentValue = 0;
+        }
+    }
+}
