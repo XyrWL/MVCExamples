d9c4cb7e2c1a5e8e7898d08d703d4104b3753ad5
XyrWL <peter.trullenque@gmail.com>
2016-04-26 13:55:52 +0200

    ViewProject added.

---
+++ ViewProject/Controllers/CarController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewProject.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: Car
+        public ActionResult Driver()
+        {
+            //Bygga modell och skicka till viewn
+            return View();
+        }
+
+        public ActionResult BrumBrum()
+        {
+            return View();
+        }
+
+        public ActionResult ViewDataTransfer()
+        {
+            ViewData["beauty"] = "Det är en häst.";
+            return View();
+        }
+
+        //[HttpPost]
+        //[AcceptVerbs(HttpVerbs.Post)]
+        [ActionName("Lisa")]
+        public ActionResult ViewBagTransfer()
+        {
+            ViewBag.Lisa = "Smurfarna är blå med vita hattar (förutom gamelsmurfen som har röd, och smurfann som inte har någon hatt).";
+            return View("ViewBagTransfer");
+        }
+
+        public ViewResult OddNumbers()
+        {
+            var oddNumbers = (ParallelEnumerable.Range(10, 40)).Where(x => x%2 != 0).ToList();
+            ViewData.Model = oddNumbers;
+            return View();
+        }
+
+    }
+}
