49db466632d55fa4717e224e1ef5e3192623b5d5
XyrWL <peter.trullenque@gmail.com>
2016-05-18 09:42:25 +0200

    AjaxWebProject part 2.

---
+++ AjaxWebProject/Controllers/CarController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxWebProject.Models;
+
+namespace AjaxWebProject.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: Car
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult _Create(int id)
+        {
+            var car = new Car {PersonId = id};
+            return PartialView(viewName: "_Create", model: car);
+        }
+    }
+}
---
+++ AjaxWebProject/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxWebProject.Models
+{
+    public class Car
+    {
+        public int PersonId { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+    }
+}
